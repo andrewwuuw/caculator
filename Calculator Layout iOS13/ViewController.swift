7abefb6c812759e34f3756f331e336cf4f14ac89
chiaqingwu <secret8419@gmail.com>
2020-07-22 07:38:44 +0800

    feat(viewcontrol): set up the actions
    numberPressed, acPressed...

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultUILabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if let resultValue = resultUILabel.text,
+            let senderValue = sender.currentTitle {
+            resultUILabel.text = (resultValue == "0") ? senderValue : resultValue + senderValue
+        }
+    }
+    
+    @IBAction func dotPressed(_ sender: UIButton) {
+        if let resultValue = resultUILabel.text, !resultValue.contains(".") {
+            resultUILabel.text = resultValue + "."
+        }
+    }
+    
+    @IBAction func percentagePressed(_ sender: UIButton) {
+        if let resultValue = convertToDouble(&resultUILabel.text) {
+            resultUILabel.text = String(resultValue/100)
+        }
+    }
+    
+    @IBAction func positiveNegativePressed(_ sender: UIButton) {
+        guard let doubleValue = convertToDouble(&resultUILabel.text) else { return }
+        guard let intValue = convertToInteger(&resultUILabel.text) else { return }
+        if let resultValue = resultUILabel.text {
+            if (resultValue.contains(".")) {
+                resultUILabel.text = String(-doubleValue)
+            } else {
+                resultUILabel.text = String(-intValue)
+            }
+        }
+    }
+    
+    @IBAction func acPressed(_ sender: UIButton) {
+        resultUILabel.text = "0"
+    }
+    
+    @IBAction func methodPressed(_ sender: UIButton) {
+        if let resultValue = resultUILabel.text,
+            let senderValue = sender.currentTitle {
+            resultUILabel.text = resultValue + senderValue
+        }
+    }
+    
+    //MARK: - Private Functions
+    private func convertToDouble<T>(_ a:inout T) -> Double? {
+        return (a as? NSString)?.doubleValue
+    }
+    
+    private func convertToInteger<T>(_ a:inout T) -> Int? {
+        return (a as? NSString)?.integerValue
+    }
 }
 
